2127b50615d64efbf7bb243feb85dace7090de13
Vera Lippgardt <vera1078@mail.ru>
2022-08-28 11:32:35 +0300

    Замена цикла while на for на примере работы с четвертым типом методов

---
+++ trening3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Четвертая группа методов: что-то принимает и что-то возвращает.
+
+string Metod4(int count, string text)
+{
+// int i = 0;
+string result = String.Empty;
+// while (i<count)
+for (int i = 0; i<count; i++) 
+{
+result = result + text;
+// i++;
+}
+return result;
+}
+string res = Metod4(10, "z ");
+Console.WriteLine(res);
